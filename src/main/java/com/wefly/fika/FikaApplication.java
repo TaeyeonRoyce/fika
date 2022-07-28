d248aa891c61555f524fe7df472cfa4cc720d81c
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-07-28 12:05:22 +0900

    initial commit

---
+++ src/main/java/com/wefly/fika/FikaApplication.java
@@ -0,0 +1,13 @@
+package com.wefly.fika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FikaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FikaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wefly/fika/FikaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wefly.fika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
